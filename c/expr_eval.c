5be46e5095e3e41ac43cf1663b0803e9dbcda29f
Chaitanya081 <192424185.simats@saveetha.com>
2025-09-29 20:00:16 +0530

    expr_eval.c

--- c/expr_eval.c
+++ c/expr_eval.c
@@ -1,23 +1,23 @@
-/* C/expr_eval.c
-   Command-line arithmetic evaluator.
-   Compile locally: gcc -o expr_eval expr_eval.c -lm
-   Usage: ./expr_eval "3 + 4*2/(1-5)^2"
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 typedef enum { T_END, T_NUMBER, T_PLUS, T_MINUS, T_MULT, T_DIV, T_POW, T_LPAREN, T_RPAREN } TokenType;
-typedef struct { TokenType type; double value; } Token;
+
+typedef struct {
+    TokenType type;
+    double value;
+} Token;
 
 const char *src;
 int pos = 0;
 Token curtok;
 int error_flag = 0;
 
-void skip_space() { while (src[pos] == ' ') pos++; }
+void skip_space() {
+    while (src[pos] == ' ') pos++;
+}
 
 void next_token() {
     skip_space();
@@ -44,19 +44,22 @@ void next_token() {
 
 /* Forward declarations */
 double parse_expression();
+
 double parse_primary() {
     if (curtok.type == T_NUMBER) { double val = curtok.value; next_token(); return val; }
-    if (curtok.type == T_LPAREN) { next_token(); double v = parse_expression(); if (curtok.type != T_RPAREN) { error_flag = 1; return 0; } next_token(); return v; }
-    error_flag = 1; return 0;
+    if (curtok.type == T_LPAREN) { next_token(); double val = parse_expression(); if (curtok.type != T_RPAREN) { error_flag = 1; return 0; } next_token(); return val; }
+    error_flag = 1;
+    return 0;
 }
+
 double parse_unary() {
     if (curtok.type == T_PLUS) { next_token(); return parse_unary(); }
     if (curtok.type == T_MINUS) { next_token(); return -parse_unary(); }
     return parse_primary();
 }
+
 double parse_pow() {
     double left = parse_unary();
-    if (error_flag) return 0;
     while (curtok.type == T_POW) {
         next_token();
         double right = parse_unary();
@@ -64,34 +67,48 @@ double parse_pow() {
     }
     return left;
 }
+
 double parse_term() {
     double left = parse_pow();
     while (curtok.type == T_MULT || curtok.type == T_DIV) {
         TokenType op = curtok.type;
         next_token();
         double right = parse_pow();
-        if (op == T_MULT) left *= right; else left /= right;
+        if (op == T_MULT) left *= right;
+        else left /= right;
     }
     return left;
 }
+
 double parse_expression() {
     double left = parse_term();
     while (curtok.type == T_PLUS || curtok.type == T_MINUS) {
         TokenType op = curtok.type;
         next_token();
         double right = parse_term();
-        if (op == T_PLUS) left += right; else left -= right;
+        if (op == T_PLUS) left += right;
+        else left -= right;
     }
     return left;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) { fprintf(stderr, "Usage: expr_eval \"expression\"\n"); return 1; }
-    src = argv[1];
-    pos = 0; error_flag = 0;
+int main() {
+    char expression[256];
+    printf("Enter arithmetic expression: ");
+    fgets(expression, sizeof(expression), stdin);
+    expression[strcspn(expression, "\n")] = 0; // remove newline
+
+    src = expression;
+    pos = 0;
+    error_flag = 0;
     next_token();
     double result = parse_expression();
-    if (!error_flag && curtok.type != T_END) { fprintf(stderr, "Syntax error: extra tokens\n"); return 1; }
-    if (!error_flag) printf("%.10g\n", result);
+
+    if (error_flag || curtok.type != T_END) {
+        printf("Syntax error in expression.\n");
+        return 1;
+    }
+
+    printf("Result: %.10g\n", result);
     return 0;
 }
